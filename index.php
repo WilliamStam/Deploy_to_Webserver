34a8036f4522829e771418aa0b2f4b0b64a15040
William Stam <awstam@gmail.com>
2012-10-25 16:01:08 +0200

    lastest

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ include_once("functions.php");
 $cfg=array();
 $cfg['base'] = dirname(dirname(__FILE__));
 
-
+$return = array();
+$return['errors'] = array();
 
 
 $link = mysql_connect('localhost', '', '');
@@ -21,65 +22,114 @@ $sql = "SELECT * FROM sites WHERE auth = '$key'";
 
 $result = mysql_query($sql, $link) or die(mysql_error());
 $row = mysql_fetch_assoc($result);
+$siteID = "";
+if (!count($row) || (!isset($row['ID']))){
+	if ($key){
+		$return['errors'][] = "Key not found";
+	} else {
+		$return['errors'][] = "No key defined";
+	};
 
-if (!count($row)){
-	exit("not authorized to do that");
+} else {
+
+	$siteID = $row['ID'];
+	if (!$row['folder']){
+		$return['errors'][] = "Folder not defined for this key";
+	} else {
+		$push = $_POST;
+		if (!isset($push['repository'])){
+			$return['errors'][] = "Not a github push";
+		} else {
+
+			$values = array(
+				"folder" => $row['folder'],
+				"branch" => (isset($_GET['branch'])) ? $_GET['branch'] : "master",
+				"auth"   => array(
+					"username" => (isset($_GET['username'])) ? $_GET['username'] : "",
+					"password" => (isset($_GET['password'])) ? $_GET['password'] : ""
+				)
+			);
+
+
+
+
+
+
+
+			$folder = $cfg['base'] . DIRECTORY_SEPARATOR . $values['folder'];
+			$return['folder'] = array(
+				"path"   => $folder,
+				"exists" => false
+			);
+			$return['git'] = false;
+
+
+			if (file_exists($folder)) {
+				$return['folder']['exists'] = true;
+			}
+			//@mkdir($folder, 0777, true);
+			if (file_exists($folder . DIRECTORY_SEPARATOR . ".git")) {
+				$return['git'] = true;
+			}
+
+			if (!$return['folder']['exists']){
+				@mkdir($folder, 0777, true);
+			}
+			$root_folder = dirname(__FILE__);
+			chdir($folder);
+
+
+			if (!$return['git']){
+				shell_exec('git init');
+			}
+
+			if (file_exists($folder)) {
+				$return['folder']['exists'] = true;
+			}
+			//@mkdir($folder, 0777, true);
+			if (file_exists($folder . DIRECTORY_SEPARATOR . ".git")) {
+				$return['git'] = true;
+			}
+
+
+			if ($return['git']){
+				$url = $push['repository']['url'].".git";
+
+
+
+				// https://username:password@github.com/WilliamStam/DeployWebserver.git
+				// https: //github.com/WilliamStam/DeployWebserver
+				$auth = "";
+				if ($values['auth']['username']){
+					$auth .= $values['auth']['username'].":";
+				}
+				if ($values['auth']['password']){
+					$auth .= $values['auth']['password'];
+				}
+
+				if ($auth) {
+					$auth = $auth."@";
+					$url = str_replace("https://","https://$auth",$url);
+				}
+
+
+
+				$return['action'] = array(
+					"reset"=>shell_exec('git reset --hard HEAD'),
+					"pull"=>shell_exec("git pull $url ".$values['branch']."  2>&1")
+				);
+			} else {
+				$return['errors'][] = "Git not setup in the folder";
+			}
+		}
+	}
 }
-if (!$row['folder']){
-	exit("no folder specified");
-}
-$values = array(
-	"folder" => $row['folder'],
-	"branch" => (isset($_GET['branch'])) ? $_GET['branch'] : "",
-	"auth"   => array(
-		"username" => (isset($_GET['username'])) ? $_GET['username'] : "",
-		"password" => (isset($_GET['password'])) ? $_GET['password'] : ""
-	)
-);
-
-$return = array(
-	"cfg" => $cfg,
-	"val" => $values,
-);
-
-$folder = $cfg['base'] . DIRECTORY_SEPARATOR . $values['folder'];
-$return['folder'] = array(
-	"path"   => $folder,
-	"exists" => false
-);
-$return['git'] = false;
-
-
-if (file_exists($folder)) {
-	$return['folder']['exists'] = true;
-}
-//@mkdir($folder, 0777, true);
-if (file_exists($folder . DIRECTORY_SEPARATOR . ".git")) {
-	$return['git'] = true;
-}
-
-if (!$return['folder']['exists']){
-	@mkdir($folder, 0777, true);
-}
-$root_folder = dirname(__FILE__);
-
-if (!$return['git']){
-	chdir($folder);
-	shell_exec('git init');
-}
-
-if (file_exists($folder)) {
-	$return['folder']['exists'] = true;
-}
-//@mkdir($folder, 0777, true);
-if (file_exists($folder . DIRECTORY_SEPARATOR . ".git")) {
-	$return['git'] = true;
-}
-
-if ($return['git']){
-	shell_exec('git reset --hard HEAD');
-}
-
-test_array($return);
+//test_array($return);
+$errors = $return['errors'];
+unset($return['errors']);
+$return = serialize($return);
+$errors = serialize($errors);
+$sql = "INSERT INTO logs (payload, errors, site) VALUES ('$return','$errors','$siteID')";
+mysql_query($sql, $link) or die(mysql_error());
 
 ?>
